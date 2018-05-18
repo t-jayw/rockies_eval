390ae865b3b2d2286bc78dae18f3ec0f4c70eeaa
tyler wood <tylerjaywood@gmail.com>
2018-05-18 10:08:40 -0600

    fixing caps, docs

--- code_eval/scrapeGamesByDate.py
+++ code_eval/scrapeGamesByDate.py
@@ -8,22 +8,23 @@ from datetime import datetime as dt
 DATE_FORMAT = '%Y-%m-%d' 
 BASE_URL="https://gd2.mlb.com/components/"+ \
           "game/mlb/year_%s/month_%s/day_%s/grid.json"
-wanted_cols = ['away_code','away_file_code', 'away_name_abbrev',
+WANTED_COLS = ['away_code','away_file_code', 'away_name_abbrev',
       'away_score', 'away_team_id', 'away_team_name', 'calendar_event_id',
       'double_header_sw', 'event_time', 'game_nbr', 'game_pk', 'game_type',
       'gameday_sw', 'group', 'home_code', 'home_file_code', 'home_name_abbrev',
       'home_score', 'home_team_id', 'home_team_name', 'id', 'ind', 'inning',
       'media_state', 'series', 'series_num', 'status', 'tbd_flag', 'top_inning',
       'venue', 'venue_id',]
-records_dir = 'daily_games_records'
-help_string = """
-Please run this script with the following command
-\n>> rip_script.py yyyy-mm-dd\n
-Where the date is formated 'yyyy-mm-dd'
-More info in readme.txt"""
+RECORDS_DIR = 'daily_games_records'
+HELP_STRING = """
+  Please run this script with the following command
+  \n>> rip_script.py yyyy-mm-dd\n
+  Where the date is formated 'yyyy-mm-dd'
+  More info in readme.txt
+"""
 
-def establish_directory(directory_name=records_dir):
-  """specs require an output directory for csvs. This
+def establish_directory(directory_name=RECORDS_DIR):
+  """specs require an output directory for csv. This
   function checks if directory defined in constants exists
   or else creates it"""
   if not os.path.exists(directory_name):
@@ -31,12 +32,12 @@ def establish_directory(directory_name=records_dir):
     print(directory_name)
 
 def print_help():
-  """Utility to handle misformated arguments"""
-  print(help_string)
+  """Print help info if things break"""
+  print(HELP_STRING)
   exit()
 
 def validate_date(date_arg):
-  """Ensures date is a date of proper format"""
+  """Ensures date arg is a date of proper format"""
   try:
     date = dt.strptime(date_arg, DATE_FORMAT)
   except ValueError:
@@ -72,13 +73,14 @@ def create_df(data):
   return df
 
 def drop_unwanted_columns(df, wanted_cols):
+  """returns a df with only the WANTED_COLS columns"""
   drop = list(set(df.columns) - set(wanted_cols))
   df = df.drop(columns = drop)
   return df
 
 def write_to_csv(df, date):
   filename = 'game_records_%s.csv'%(date)
-  path = os.path.join(records_dir, filename)
+  path = os.path.join(RECORDS_DIR, filename)
   df.to_csv(path)
   print("csv written to "+path)
 
@@ -102,6 +104,6 @@ if __name__ == "__main__":
   url = format_request(date)
   data = request_data(url)
   df = create_df(data)
-  df = drop_unwanted_columns(df, wanted_cols)
+  df = drop_unwanted_columns(df, WANTED_COLS)
   write_to_csv(df, date)
 
